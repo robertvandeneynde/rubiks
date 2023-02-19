fae9ec63aea5769ce6450518b86f3f59dcb99c6d
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2023-02-19 22:57:38 +0100

    Allow more cases at once in random_bh_case

--- bld/random_bh_case.py
+++ bld/random_bh_case.py
@@ -17,7 +17,7 @@ PROBA = {
 
 scheme = SCHEME[input("[s]peffz|[k]klise ").lower()]
 buffer = input("Buffer ? (Letter) ").upper()
-proba = PROBA[input("proba ? [c]ategory|[l]inear").lower()]
+proba = PROBA[input("proba ? [c]ategory|[l]inear ").lower()]
 
 filename = f"generated/comms_buffer_changed_{scheme}_buffer_{buffer}.json"
 try:
@@ -29,21 +29,26 @@ except FileNotFoundError as e:
 
 if proba == 'category':
     categories = list(set(d[2] for d in data))
-    def pick(n=1):
+    def pick_one():
         cat = random.choice(categories)
-        if n == 1:
-            return random.choice([d for d in data if d[2] == cat])
-        else:
-            return [pick(1) for i in range(n)]
+        return random.choice([d for d in data if d[2] == cat])
+     
+    def pick(n=1):
+        return [pick_one() for i in range(n)]
         
 elif proba == 'linear':
     def pick(n=1):
         return random.sample(data, n)
 
-p = pick(1)
-answer = input(p[0] + "? ")
-correct_answer = p[2]
-if answer.upper() == answer.upper():
-    print("Correct")
-else:
-    print("False")
+p = pick(10)
+answers = input(' '.join(p[0] for p in p) + " ?\n")
+correct_answers = ' '.join(p[2] for p in p)
+print(correct_answers)
+s = 0
+for i, (p, a, c) in enumerate(zip(p, answers.split(), correct_answers.split())):
+    if a.upper() == c.upper():
+        print(p[0], "==", a.upper())
+        s += 1
+    else:
+        print(p[0], "//", a.upper(), "==", c.upper())
+print("{}/{}".format(s, 10))
